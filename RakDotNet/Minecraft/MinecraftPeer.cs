f8e76f9cf9582a76de225a0fa4d8d9e4db5eaa56
hiroki19990625 <hiroki19990625@gmail.com>
2019-05-13 20:24:20 +0900

    バージョンを更新

--- RakDotNet/Minecraft/MinecraftPeer.cs
+++ RakDotNet/Minecraft/MinecraftPeer.cs
@@ -269,9 +269,6 @@ namespace RakDotNet.Minecraft
             CustomPacket pk =
                 Server.Socket.PacketIdentifier.GetPacketFormId(MinecraftServer.CUSTOM_PACKET_0) as CustomPacket;
 
-            Logger.Info(pk.PacketId);
-            Logger.Info(SendSequenceNumber);
-
             pk.SequenceId = SendSequenceNumber++;
             pk.Packets = new[] {packet};
             pk.EndPoint = endPoint;
